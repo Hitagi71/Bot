cd912adb10eef309f9449117beba190c49995344
Vinicius Gadelha dos Santos <vinibr242@hotmail.com>
2021-01-21 22:53:54 -0300

    fetchNow

--- commands/w.js
+++ commands/w.js
@@ -5,38 +5,46 @@ module.exports = {
     "name": "w",
     "description": "roleta a waifu",
     execute(message, args) {
-        let id = 0
-        do{
-            id = Math.floor(Math.random() * 52) + 1
-        }while(id<33)
-        fetch(`http://localhost:3000/character/${id}`)
-        .then((resp) => {
-            let contentType = resp.headers.get("content-type");
-            if (contentType && contentType.indexOf("application/json") !== -1) {
-                return resp.json().then(function (json) {
-                    fetch(json.character.photos)
-                    .then((photos) => photos.json())
-                    .then((resp) => {
-                        let photos = [];
-                        for(let i = 0; i < resp.images.length; i++){
-                            photos[i] = resp.images[i].photo
-                        }
+        var resultFound = false
+        var fetchNow = function(){
+            var id = Math.floor(Math.random() * 100) + 1
+            fetch(`http://localhost:3000/character/${id}`)
+            .then((resp) => {
+                
+                    let contentType = resp.headers.get("content-type");
+                    if (contentType && contentType.indexOf("application/json") !== -1) {
+                        return resp.json().then(function (json) {
+                            if(json.message !== 'id not found'){
+                                resultFound = true
+                                fetch(json.character.photos)
+                                .then((photos) => photos.json())
+                                .then((resp) => {
+                                    let photos = [];
+                                    for(let i = 0; i < resp.images.length; i++){
+                                        photos[i] = resp.images[i].photo
+                                    }
 
-                        const attachment = new Discord.MessageAttachment(photos[0], 'image.png')
-                        const exampleEmbed = new Discord.MessageEmbed()
-                        .setColor('#FD3F96')
-                        .attachFiles(attachment)
-                        .setTitle(json.character.name)
-                        .setDescription(json.character.anime_name)
-                        .setImage('attachment://image.png')
-                        .setTimestamp()
-                            
-                        message.channel.send(exampleEmbed).then( async exampleEmbed => {
-                            await exampleEmbed.react('💖');
-                        });  
-                    })
-                })
-            }
-        })
+                                    const attachment = new Discord.MessageAttachment(photos[0], 'image.png')
+                                    const exampleEmbed = new Discord.MessageEmbed()
+                                    .setColor('#FD3F96')
+                                    .attachFiles(attachment)
+                                    .setTitle(json.character.name)
+                                    .setDescription(json.character.anime_name)
+                                    .setImage('attachment://image.png')
+                                    .setTimestamp()
+                                        
+                                    message.channel.send(exampleEmbed).then( async exampleEmbed => {
+                                        await exampleEmbed.react('💖');
+                                    });  
+                                })
+                            }else{
+                                console.log(json.message)
+                                fetchNow()
+                            }
+                        })
+                    }
+            })
+        }
+        fetchNow()
     }
 }
