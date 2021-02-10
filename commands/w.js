ee9d7d149562c5334bc455a2c75a1bd97efc1a00
Hitagi71 <andre.gomes7110@gmail.com>
2021-02-10 20:58:17 -0300

    Nova Url da Api

--- commands/w.js
+++ commands/w.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 const Discord = require('discord.js');
+const baseApiUrl = require('../global').baseApiUrl
 
 module.exports = {
     name: "w",
@@ -8,30 +9,30 @@ module.exports = {
     execute(message, args) {
         var resultFound = false
         var fetchNow = function(){
-            var id = Math.floor(Math.random() * 150) + 1
-            fetch(`http://localhost:3000/character/${id}`)
+            let aleatorio = Math.floor(Math.random() * 3)
+            let id = aleatorio * 10 + 1
+            fetch(`${baseApiUrl}/character/${id}`)
             .then((resp) => {
-                
                     let contentType = resp.headers.get("content-type");
                     if (contentType && contentType.indexOf("application/json") !== -1) {
                         return resp.json().then(function (json) {
                             if(json.message !== 'id not found'){
                                 resultFound = true
-                                fetch(json.character.photos)
+                                fetch(json.character.images)
                                 .then((photos) => photos.json())
                                 .then((resp) => {
                                     let photos = [];
                                     for(let i = 0; i < resp.images.length; i++){
-                                        photos[i] = resp.images[i].photo
+                                        photos[i] = resp.images[i].url
                                     }
 
-                                    const attachment = new Discord.MessageAttachment(photos[0], 'image.png')
+                                    console.log(photos[0])
+
                                     const exampleEmbed = new Discord.MessageEmbed()
                                     .setColor('#FD3F96')
-                                    .attachFiles(attachment)
                                     .setTitle(json.character.name)
                                     .setDescription(json.character.anime_name)
-                                    .setImage('attachment://image.png')
+                                    .setImage(photos[0])
                                     .setTimestamp()
                                         
                                     message.channel.send(exampleEmbed).then( async exampleEmbed => {
---
+++ global.js
@@ -0,0 +1,3 @@
+const baseApiUrl = 'https://eri-api.herokuapp.com'
+
+exports.baseApiUrl = baseApiUrl
